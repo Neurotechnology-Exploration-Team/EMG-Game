2f1e76dad9ad60bf90ed5dd677c1b9121e011672
MJE10 <michaeljelia@gmail.com>
2022-02-12 18:44:46 -0500

    Fix boardcontroller dll issue, prepare c# for searching serial ports

--- Assets/Car EMG/Scripts/CarBCIReader2.cs
+++ Assets/Car EMG/Scripts/CarBCIReader2.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
 using UnityEngine;
 
 using brainflow;
 
 public class CarBCIReader2 : MonoBehaviour
 {
-    private const string SerialPort = "COM7";
+    private const string serialPort = null;
     private const int NumSamplesPerInput = 500;
     private const int BoardID = 0;
     
@@ -15,32 +17,36 @@ public class CarBCIReader2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // enable debug info
         BoardShim.enable_dev_board_logger();
 
-        var inputParams = new BrainFlowInputParams
+        if (serialPort != null)
         {
-            serial_port = SerialPort
-        };
+            var inputParams = new BrainFlowInputParams
+            {
+                serial_port = serialPort
+            };
 
-        _boardShim = new BoardShim(BoardID, inputParams);
-        try
-        {
-            _boardShim.prepare_session();
-        }
-        catch (BrainFlowException e)
-        {
-            Debug.Log("Do you have the wrong port, or is the device plugged in, or could the device be taken up by other programs?");
-            Debug.LogError(e);
-        }
-        _boardShim.start_stream(450000, "file://file_stream.csv:w");
+            _boardShim = new BoardShim(BoardID, inputParams);
+            try
+            {
+                _boardShim.prepare_session();
+            }
+            catch (BrainFlowException e)
+            {
+                Debug.Log("Do you have the wrong port, or is the device plugged in, or could the device be taken up by other programs?");
+                Debug.LogError(e);
+            }
+            _boardShim.start_stream(450000, "file://file_stream.csv:w");
 
-        Debug.Log("OpenBCI initialization complete");
+            Debug.Log("OpenBCI initialization complete");   
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        double[,] data = GetRawData();
     }
     private double[,] GetRawData()
     {
