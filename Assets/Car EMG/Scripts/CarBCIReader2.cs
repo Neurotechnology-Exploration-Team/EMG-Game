dac0a7f52b2181b662178add9321412c54e5cdfd
MJE10 <michaeljelia@gmail.com>
2022-02-17 14:43:53 -0500

    Added search for serial ports and error handling

--- Assets/Car EMG/Scripts/CarBCIReader2.cs
+++ Assets/Car EMG/Scripts/CarBCIReader2.cs
@@ -1,70 +1,136 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.IO.Ports;
-using System.Linq;
 using UnityEngine;
 
 using brainflow;
 
 public class CarBCIReader2 : MonoBehaviour
 {
-    private const string serialPort = "COM7";
+    public bool verbose;
+    
+    private enum ConnectionStatus
+    {
+        Disconnected,
+        Connecting,
+        Connected
+    }
+    
     private const int NumSamplesPerInput = 500;
     private const int BoardID = 0;
     
-    private static BoardShim _boardShim;
+    private string serialPort;
+    private BoardShim boardShim;
+    private ConnectionStatus connectionStatus = ConnectionStatus.Disconnected;
 
     // Start is called before the first frame update
     void Start()
     {
+        AttemptConnect();
+        
+        if (connectionStatus == ConnectionStatus.Disconnected) Debug.Log("No board detected.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (connectionStatus == ConnectionStatus.Connected)
+        {
+            double[,] data = GetRawData();
+        }
+    }
+
+    private bool AttemptConnect()
+    {
         // enable debug info
         BoardShim.enable_dev_board_logger();
 
-        if (serialPort == null) return;
+        if (serialPort == null)
+        {
+            if (verbose) Debug.LogWarning("Warning: No serial port detected. Attempting to search for board...");
+            for (int i = 0; i < 10; i++)
+            {
+                if (AttemptConnectSerial("COM" + i))
+                {
+                    serialPort = "COM" + i;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        else
+        {
+            return AttemptConnectSerial(serialPort);
+        }
+    }
+
+    private bool AttemptConnectSerial(string attemptSerialPort)
+    {
+        connectionStatus = ConnectionStatus.Connecting;
         
         var inputParams = new BrainFlowInputParams
         {
             serial_port = serialPort
         };
 
-        _boardShim = new BoardShim(BoardID, inputParams);
+        boardShim = new BoardShim(BoardID, inputParams);
         try
         {
-            _boardShim.prepare_session();
+            boardShim.prepare_session();
             
-            _boardShim.start_stream(450000, "file://file_stream.csv:w");
+            boardShim.start_stream(450000, "file://file_stream.csv:w");
 
-            Debug.Log("OpenBCI initialization complete");   
+            Debug.Log("OpenBCI initialization complete on " + attemptSerialPort);
+            return true;
         }
         catch (BrainFlowException e)
         {
-            Debug.Log("Do you have the wrong port, or is the device plugged in, or could the device be taken up by other programs?");
-            Debug.LogError(e);
+            switch (e.Message)
+            {
+                case "UNABLE_TO_OPEN_PORT_ERROR:2":
+                    if (verbose) Debug.LogWarning("Warning, board not available on " + attemptSerialPort + ": UNABLE_TO_OPEN_PORT_ERROR:2");
+                    connectionStatus = ConnectionStatus.Disconnected;
+                    return false;
+                case "GENERAL_ERROR:17":
+                    if (verbose) Debug.LogWarning("Warning, board not available on " + attemptSerialPort + ": GENERAL_ERROR:17");
+                    connectionStatus = ConnectionStatus.Disconnected;
+                    return false;
+                default:
+                    Debug.Log("Unknown message: " + e.Message);
+                    throw;
+            }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        double[,] data = GetRawData();
     }
     private double[,] GetRawData()
     {
+        if (connectionStatus != ConnectionStatus.Connected)
+        {
+            Debug.LogWarning("Warning: attempt to collect data when board is not connected");
+            return null;
+        }
         try
         {
-            return _boardShim.get_current_board_data(NumSamplesPerInput);
+            return boardShim.get_current_board_data(NumSamplesPerInput);
         }
-        catch (BrainFlowException)
+        catch (BrainFlowException e)
         {
-            Debug.Log("Do you have the wrong port, or is the device plugged in, or could the device be taken up by other programs?");
-            Destroy(gameObject);
-            throw;
+            if (e.Message.Equals("BOARD_NOT_CREATED_ERROR:15"))
+            {
+                Debug.Log("Warning: attempt to collect data when board is believed to be connected but actually is not.");
+                connectionStatus = ConnectionStatus.Disconnected;
+                return null;
+            }
+            else
+            {
+                Debug.LogError("Error: unknown. Terminating session and throwing error: " + e.Message);
+                Destroy(gameObject);
+                throw;
+            }
         }
     }
 
     private void OnDestroy()
     {
-        _boardShim.stop_stream();
-        _boardShim.release_session();
+        if (connectionStatus == ConnectionStatus.Disconnected) return;
+        boardShim.stop_stream();
+        boardShim.release_session();
     }
 }
